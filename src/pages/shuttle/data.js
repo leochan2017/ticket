a1cb50a79c4f2cf8c7d60588ec10261409c89d4c
Leo <leochan2017@gmail.com>
2022-09-30 18:28:51 +0800

    chore: Update address

--- src/pages/shuttle/data.js
+++ src/pages/shuttle/data.js
@@ -6,9 +6,9 @@ const count = window.location.host.indexOf('localhost') !== -1 ? 1 : 4
 
 const data = {
   count,
-  address1: '(A线)幸福誉3/5期',
+  address1: '(A线)幸福悦3/5期',
   address2: '宏发',
-  address3: '幸福誉',
+  address3: '幸福悦3/5期',
   price: 10,
   priceUnit: '元',
   personCount: 3,
