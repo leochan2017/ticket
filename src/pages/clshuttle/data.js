6ac57b95cf923cc7d05786071cc6d8bcf2c7efae
Leo <leochan2017@gmail.com>
2023-12-10 16:09:57 +0800

    chore: Update data from CL

--- src/pages/clshuttle/data.js
+++ src/pages/clshuttle/data.js
@@ -17,10 +17,10 @@ const data = {
 
   departTime1: '7:02',
   depart1: '幸福悦3/5期',
-  destination1: '长隆地铁站G出口',
+  destination1: '广州南站P8停车场',
 
   departTime2: '19:02',
-  depart2: '长隆地铁站G出口',
+  depart2: '广州南站P8停车场',
   destination2: '幸福悦3/5期',
 
   needHelp: '客服：13763305584(客服号),18664606665(小罗)(微信同号)',
